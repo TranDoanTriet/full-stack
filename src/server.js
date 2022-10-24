df58af7129f2d9826d3098ec9b86a0ba4ab60a44
TranDoanTriet <trandoantriet@gmail.com>
2022-10-24 17:25:35 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from  "./config/viewEngine"
+import initWebRoutes from "./routes/web"
+require("dotenv").config()
+
+let app = express(); // la mot instant cua express
+
+//config app
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+viewEngine(app);
+initWebRoutes(app);
+
+let port = process.env.PORT || 6969; // port === undefined -> port = 6969
+app.listen(port, () => {
+    //callback
+    console.log("back end nodejs is running on port: " + port)
+})
